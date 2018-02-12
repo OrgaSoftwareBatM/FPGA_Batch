10560d230a4209841693c42abad018503bc1e815
manip batm <emanuel.chanrion@neel.cnrs.fr>
2018-02-12 10:13:54 +0100

    rtq
    rtq

---
+++ Python/PyPlot/20180212-RTtest/csvimport.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 08:56:54 2018
+
+@author: manip.batm
+"""
+
+from analysis import spec
+#%matplotlib inline
+data = spec(fname='20170205-Monitor_History-Weekend.csv')
